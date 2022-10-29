d8c2c0090e38c86c397e2914a4f6a0bbea8d8cf8
amanjhaaa <66066010+amanjhaaa@users.noreply.github.com>
2022-10-29 13:09:30 +0530

    Database connectivity with mongodb

---
+++ app.py
@@ -0,0 +1,168 @@
+
+# from crypt import methods
+from flask import Flask,render_template,request
+app = Flask(__name__)#interface between webserver and web application
+
+
+@app.route("/" , methods = ['GET','POST'])
+def home():
+    return render_template("registration.html")  
+
+@app.route('/res',methods=['GET',"POST"])
+def res():
+    if request.method == "POST":
+        firstname = request.form.get('firstname')
+    return render_template('result1.html', fname=firstname)   
+
+
+
+
+@app.route('/login/', methods=['GET',"POST"])
+def ncd():
+    return render_template('ncd.html')
+
+@app.route('/result',methods=['GET',"POST"])
+def result():
+    if request.method == "POST":
+        count = 0
+
+# getting the value for age
+
+        while True :
+            try:
+                age = request.form.get('age')
+                if (int(age)>=30 and int(age)<=39):
+                    count+= 0
+                    break
+                elif(int(age)>=40 and int(age)<=49):
+                    count = count+1
+                    break
+                elif(int(age)>=50 and int(age)<=59):
+                    count+=2
+                    break
+                elif (int(age)>=60):
+                    count+=3
+                    break
+                else:
+                    print("Enter the valid age")  
+            except ValueError:
+                continue
+        
+        while True:
+            try:
+                smoke = request.form.get('smoke')
+                # if smoke in lis1:
+                if (str(smoke).lower() == "never"):
+                    count = count+0
+                    break
+                elif((str(smoke).lower() =="used to consume in past")or (str(smoke).lower() =="sometime now")):
+                    count+=1
+                    break
+                elif (str(smoke).lower() =="daily"):
+                    count+=2
+                    break
+                else:
+                    print("Enter the valid result for smoking.")  
+            except TypeError:
+                continue
+        while True:
+            try:
+                alcohol =request.form.get('alcohol')
+
+                yes_choices = ['yes', 'y']
+                no_choices = ['no', 'n']
+
+                if (str(alcohol).lower() in yes_choices):
+                    count  = count+0
+                    break
+                elif (str(alcohol).lower() in no_choices):
+                    count = count+1
+                    break
+                else:
+                    print("Give the alcohol choice")
+                    break
+            except TypeError:
+                continue    
+
+        while True:
+            try:
+                gender_m = request.form.get('male')
+                gender_f = request.form.get('female')
+                measurement = request.form.get('measurement')
+                print(gender_m,gender_f,measurement)
+                if(str(gender_m).lower() == "on"):
+                    if (int(measurement)<=90):
+                        count = count+0
+                        break
+                    elif (int(measurement)>90 and int( measurement)<=100):
+                        count = count+1
+                        break
+                    elif (int(measurement)>100):
+                        count+=2
+                        break
+                elif(str(gender_f).lower() == "on"):
+                    if (int(measurement)<=80):
+                        count = count+0
+                        break
+                    elif (int(measurement)>80 and int(measurement)<=90):
+                        count = count+1
+                        break
+                    elif (int(measurement)>90):
+                        count+=2    
+                        break
+                else:
+                    print("Give the correct measurement input")
+                    break
+            except ValueError:
+                continue
+
+        while True:
+            try:
+                physical =  request.form.get('physical')
+
+
+                if (int(physical)>=150):
+                    count = count+0
+                    break
+                else:
+                    count = count+1
+                    break
+            except ValueError:
+                continue
+        # print("count 5",count)
+
+        while True:
+            try:
+                disease_family_hist =  request.form.get('fam')
+
+                yes_choices = ['yes', 'y']
+                no_choices = ['no', 'n']
+                if (str(disease_family_hist).lower() in no_choices):
+                    count = count+0
+                    break
+                elif (str(disease_family_hist).lower() in yes_choices):
+                    count = count+2
+                    break
+                else:
+                    print("give the correct family history disease input")
+                    break
+            except TypeError:
+                continue
+        add = count
+        print(add)
+        res=""
+        if count>4:
+            res="you need screening" 
+        else:
+            res="No screening needed"
+
+        return render_template('result1.html', add1=add,prescription=res)
+    return render_template('result1.html', add1="result not found in session.")
+
+@app.route('/back',methods=['GET',"POST"])  
+def back():  
+    if request.method == 'POST':
+        return render_template("ncd.html");  
+if __name__ == '__main__':
+    app.run(debug = True)  
+    
---
+++ app1.py
@@ -0,0 +1,200 @@
+from flask import Flask,render_template,request
+from pymongo import MongoClient
+
+# import json
+app = Flask(__name__)#interface between webserver and web application
+
+
+
+firstname = " "
+add = 0
+result = " "
+
+@app.route("/" , methods = ['GET','POST'])
+def home():
+    return render_template("registration.html")
+    # def create_user():
+    # try:
+    #     user = {"name":request.form['fname'],
+    #     "lastname":request.form['lname'],
+    #     "email":request.form['email'],
+    #     "gender":request.form['gender'],
+    #     "pincode":request.form["pincode"]
+    #     }
+    #     dbResponse = db.users.insert_one(user)
+    #     print (dbResponse.inserted_id)
+    #     return Response(
+    #         response = json.dumps(
+    #             {"message":"user created",
+    #             "id":f"{dbResponse.inserted_id}"
+    #             }
+    #         ),
+    #         status = 200,
+    #         mimetype = "application/json"
+    #         )
+    # except Exception as e:
+    #     print(e)
+      
+
+# route to get data from html form and insert data into database
+@app.route('/registration', methods=["GET", "POST"])
+def registration():
+    global firstname
+    middlename = " "
+    lastname = " "
+    email = " "
+    gender = " "
+    birthday = " "
+    pincode = " "
+    if request.method == "POST":
+    
+        firstname = request.form['fname']
+        middlename = request.form['mname']
+        lastname = request.form['lname']
+        email =  request.form['email']
+        gender = request.form['gender']
+        birthday = request.form['birthday']
+        pincode = request.form["pincode"]
+        
+        Collection.insert_one(
+            {"firstname" : firstname,
+        'middlename': middlename,
+        "lastname":lastname,
+        "email":email,
+        "gender":gender,
+        "birthday":birthday,
+        "pincode":pincode}
+        )
+    # print(data)
+    return render_template("ncd1.html",fname = firstname,mname= middlename,lname =  lastname,email1 = email,gender1 = gender,birthday1 = birthday,pincode1 = pincode)
+
+
+@app.route('/res',methods=['GET',"POST"])
+def res():
+    # if request.method == "POST":
+        # firstname = request.form.get('firstname')
+    return render_template('result1.html')   
+
+
+
+
+@app.route('/login/', methods=['GET',"POST"])
+def login():
+    # data = {}
+    # if request.method == "POST":
+         
+    #     data['firstname']=  request.form['fname']    
+    #     data['middlename'] = request.form['mname']
+    #     data['lastname'] = request.form['lname']
+    #     data['email'] = request.form['email']
+    #     data['gender'] = request.form['gender']
+    #     data['birthday'] = request.form['birthday']
+    #     data['pincode'] = request.form["pincode"]
+    #     Collection.insert_one(data)
+       
+            # data['middlename'] = request.form['mname']
+        # data['lastname'] = request.form['lname']
+        # data['email'] = request.form['email']
+        # data['gender'] = request.form['gender']
+        # data['birthday'] = request.form['birthday']
+        # data['pincode'] = request.form["pincode"]
+        # }
+    
+    
+    return render_template('ncd1.html')
+
+@app.route('/result',methods=['GET',"POST"])
+def result():
+    if request.method == "POST":
+        count = 0
+
+# getting the value for age
+
+        while True :
+            age = ""
+            smoke = ""
+            alcohol = ""
+            measurement = ""
+            physical = ""
+            disease_family_hist = ""
+        
+            age = request.form['age']
+            
+            smoke = request.form['smoke']
+            
+            alcohol =request.form['alcohol']
+
+            
+            measurement = request.form['measurement']
+            
+            physical =  request.form['physical']
+
+
+
+            disease_family_hist =  request.form['history']
+        
+            # age = request.form.get('age')
+            
+            # smoke = request.form.get('smoke')
+            
+            # alcohol =request.form.get('alcohol')
+            # measurement = request.form.get('measurement')
+            
+            # physical =  request.form.get('physical')
+
+
+
+            # disease_family_hist =  request.form.get('history')
+            Collection.update_one(
+                {"firstname":firstname},
+                
+            {"$set": {"age" :age,
+        'smoke': smoke,
+        "alcohol":alcohol,
+        "measurement":measurement,
+        "physical":physical,
+        
+        "disease_family_hist":disease_family_hist}}
+       
+        )
+        
+            count = int(age)+int(smoke)+int(alcohol)+int(measurement)+int(physical)+int(disease_family_hist)
+            
+            global add 
+            add = count
+            print(add)
+            global result
+            if count>4:
+                result="you need screening" 
+                Collection.update_one(
+                {"firstname":firstname},
+                {"$set": {"total_count" :add,"result" :result}})
+            else:
+                result="No screening needed"
+                Collection.update_one(
+                {"firstname":firstname},{"$set": {"total_count" :add,"result" :result}})
+
+
+            return render_template('result1.html', add1=add,prescription=result)
+    return render_template('result1.html', add1="result not found in session.")
+
+@app.route('/back',methods=['GET',"POST"])  
+def back():  
+    if request.method == 'POST':
+        return render_template("ncd1.html");  
+
+        
+if __name__ == '__main__':
+
+    try:
+        client = MongoClient("mongodb://localhost:27017")
+        db = client['patientData']
+        Collection = db["mysamecollectionforpatient"]
+        # client.server_info() #trigger exception if it cannot connect to database
+        
+    except Exception as e:
+        print(e)
+        print("Error - Cannot connect to database")
+
+    # print(data.fname)
+    app.run(debug=True)
