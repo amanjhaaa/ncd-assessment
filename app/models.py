148ef0ea477cee2c0311851cb8f78041de2034c3
amanjhaaa <66066010+amanjhaaa@users.noreply.github.com>
2022-11-04 18:29:03 +0530

    ncd assessment with mvc arch

--- app/models.py
+++ app/models.py
@@ -1,16 +1,15 @@
-from flask import Flask,render_template,request
-from flask_mysqldb import MySQL
+from flask import Flask, render_template, request, url_for, redirect
+from app import app
+from flask_mysqldb import MySQL 
+import MySQLdb.cursors
+
 from random import randint
-
-
-# import json
-app = Flask(__name__)#interface between webserver and web application
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'aman1'
 app.config['MYSQL_PASSWORD'] = '1234'
 app.config['MYSQL_DB'] = 'aman'
- 
+
 
 mysql = MySQL(app)
 
@@ -25,41 +24,6 @@ measurement = ""
 physical = ""
 disease_family_hist = ""
 patient_id = " "
-
-@app.route("/" , methods = ['GET','POST'])
-def home():
-    return render_template("index.html")
-
-
-@app.route("/templates/about.html")
-def about():
-    return render_template("about.html")
-
-
-
-@app.route("/templates/search.html")
-def search():
-    return render_template("search.html")
-
-
-
-
-
-@app.route("/templates/contact.html")
-def contact():
-    return render_template("contact.html")
-
-
-    
-@app.route("/templates/services.html")
-def services():
-    return render_template("services.html")
-
-
- 
-@app.route("/templates/registration.html")
-def registration1():
-    return render_template("registration.html")
 
 
 # route to get data from html form and insert data into database
@@ -97,19 +61,7 @@ def registration():
     return render_template("ncd1.html",patientid = patient_id , fname = firstname,mname= middlename,lname =  lastname,email1 = email,gender1 = gender,birthday1 = birthday,pincode1 = pincode)
 
 
-@app.route('/res',methods=['GET',"POST"])
-def res():
- 
-    return render_template('result1.html')   
 
-
-
-
-@app.route('/login/', methods=['GET',"POST"])
-def login():
- 
-    
-    return render_template('ncd1.html')
 
 @app.route('/result1',methods=['GET',"POST"])
 def result1():
@@ -169,19 +121,6 @@ def result1():
             return render_template('result1.html', add1=add,prescription=result)
     return render_template('result1.html', add1="result not found in session.")
 
-@app.route('/back',methods=['GET',"POST"])  
-def back():  
-    if request.method == 'POST':
-        return render_template("ncd1.html");  
-
-#back 1 is used for contact.html
-@app.route('/back1',methods=['GET',"POST"])  
-def back1():  
-    if request.method == 'POST':
-        return render_template("index.html");  
-
-
-
 
 
 @app.route("/searching1", methods=['GET', 'POST'])
@@ -196,8 +135,3 @@ def searching():
 
         print(data)
         return render_template("search.html",data = data)
-       
-if __name__ == '__main__':
-
-  
-    app.run(debug=True)
